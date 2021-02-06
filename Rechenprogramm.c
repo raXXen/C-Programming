3756e18af2665b993d45a9ebe39992a23d587407
raXXen <Bjar6492@web.de>
2021-02-06 21:14:25 +0100

    Tried to fix Rechenprogramm.c

--- Rechenprogramm.c
+++ Rechenprogramm.c
@@ -5,27 +5,26 @@ int main(void)
 	char wahl;
 	double wert1, wert2, ergebniss;
 
-	printf("Bitte geben Sie zwei Flieskommawerte ein: ");
-	scanf("%lf.%lf", &wert1, &wert2);
+	printf("Bitte geben Sie zwei Flieskommawerte ein: \n");
+	scanf("%lf.%lf \n", &wert1, &wert2);
 
 	printf("Mit welchem Operator soll die Rechnung durgefuehrt werden\n");
 	printf("A)ddition\nS)ubtraktion\n");
 	printf("M)ultiplikation\nD)ivision\n");
 	printf("Ihre Wahl: \n");
 
-	fflush(stdin);
 	wahl = getchar();
 
-	if (wahl == 'A' || wahl == 'a'){
+	if ( (wahl == 'A') || (wahl == 'a') ){
 		ergebniss = wert1 + wert2;
 	}
-	else if (wahl == 'S' || wahl == 's'){
+	else if ( (wahl == 'S') || (wahl == 's') ){
 		ergebniss = wert1 - wert2;
 	}
-	else if (wahl == 'M' || wahl == 'm'){
+	else if ( (wahl == 'M') || (wahl == 'm') ){
 		ergebniss = wert1 * wert2;
 	}
-	else if (wahl == 'D' || wahl == 'd'){
+	else if ( (wahl == 'D') || (wahl == 'd') ){
 		ergebniss = wert1 / wert2;
 	}
 
